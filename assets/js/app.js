3b0bdccf01d06f077448d9d5957e2bd190bcb2f6
DimK10 <james_k23@hotmail.gr>
2020-05-25 12:09:47 +0300

    Added coreui theme to react -- bundles css and js, but also yarn add @coreui/coreui so that jquery is working?

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import '@coreui/coreui'
 
 // class App extends React.Component {
 //     constructor() {
@@ -16,6 +16,20 @@ import ReactDOM from 'react-dom';
 //     }
 // }
 
-const App = () => (<div>Hello from React!</div>);
+const App = () => (<div>
+    <div className="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+    </div><button type="button" className="btn btn-success">Success</button>
+    <div className="dropdown">
+        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                data-toggle="dropdown" aria-expanded="false">
+            Dropdown button
+        </button>
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a className="dropdown-item" href="#">Item</a>
+            <a className="dropdown-item" href="#">Another Item</a>
+            <a className="dropdown-item" href="#">One more item</a>
+        </div>
+    </div></div>);
 
 ReactDOM.render(<App />, document.getElementById('root'));
