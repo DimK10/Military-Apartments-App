3c768abe81739dcbae8e5fcc792790b0c537f074
DimK10 <james_k23@hotmail.gr>
2020-05-27 17:24:20 +0300

    PersonInArmy fixture ready

--- src/DataFixtures/PersonInArmyFixture.php
+++ src/DataFixtures/PersonInArmyFixture.php
@@ -2,16 +2,94 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\PersonInArmy;
+use App\Entity\Scoring;
+use App\Entity\Tenant;
+use App\Entity\Unit;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use PhpParser\Node\Expr\Array_;
 
-class PersonInArmyFixture extends Fixture
+class PersonInArmyFixture extends BaseFixture implements DependentFixtureInterface
 {
-    public function load(ObjectManager $manager)
+    public function loadData(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+
+        $this->createMany(22, 'peopleInArmy', function ($i) {
+            $randomFirstName= array([
+                0 => 'Δημήτριος',
+                1 => 'Γεώργιος',
+                2 => 'Κωνσταντίνος',
+                3 => 'Πέτρος',
+                4 => 'Χρήστος',
+                5 => 'Γιάννης'
+            ]);
+
+            $randomSurname = [
+                0 => 'Παλαιολόγου',
+                1 => 'Ιωάννου',
+                2 => 'Παπαπαναγιώτου',
+                3 => 'Παπαδόπουλος',
+                4 => 'Δημητρίου',
+                5 => 'Γεωργίου',
+                6 => 'Κουμπούρης',
+                7 => 'Αναστασίου',
+                8 => 'Αβραμίδης',
+                9 => 'Αγγελίδης'
+            ];
+
+            $randomRanks = [
+                0 => 'Ατγος',
+                1 => 'Υπτγος',
+                2 => 'Τξχος',
+                3 => 'Σχης',
+                4 => 'Ανχης',
+                5 => 'Τχης',
+                6 => 'Λγος',
+                7 => 'Υπλγος',
+                8 => 'Ανθλγος',
+                9 => 'Ανθστης',
+                10 => 'Αλχιας',
+                11 => 'Επχίας',
+                12 => 'Λχιας',
+                13 => 'Δνεας'
+            ];
+
+            $randomSpecialty = [
+                0 => '(ΔΒ)',
+                1 => '(ΜΧ)',
+                2 => '(ΠΒ)',
+                3 => '(ΠΖ)',
+                4 => '(ΤΘ)',
+                5 => '(ΤΧ)',
+                6 => '(ΥΓ)',
+                7 => '(EM)'
+            ];
+
+            $personInArmy = new PersonInArmy();
+            $personInArmy->setFirstname((string)$randomFirstName[rand(0, 5)]);
+            $personInArmy->setSurname((string)$randomSurname[rand(0, 9)]);
+            $personInArmy->setRank((string)$randomRanks[rand(0, 13)]);
+            $personInArmy->setSpecialty((string)$randomSpecialty[rand(0, 7)]);
+
+            $personInArmy->setUnit($this->getRandomReference('units'));
+            $personInArmy->setTenant($this->getRandomReference('tenants'));
+            $personInArmy->setScoring($this->getRandomReference('scorings'));
+        });
 
         $manager->flush();
     }
+
+    public function getDependencies()
+    {
+        return [
+            Tenant::class,
+            Unit::class,
+            Scoring::class
+        ];
+    }
+
+
 }
