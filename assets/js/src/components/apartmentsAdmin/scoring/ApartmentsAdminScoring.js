f85541cfc9f2da5674fc67ff2fea77d96034e5f1
DimK10 <james_k23@hotmail.gr>
2020-06-14 20:26:55 +0300

    Some ui fixes

--- assets/js/src/components/apartmentsAdmin/scoring/ApartmentsAdminScoring.js
+++ assets/js/src/components/apartmentsAdmin/scoring/ApartmentsAdminScoring.js
@@ -16,73 +16,78 @@ const ApartmentsAdminScoring = ({ getScores, scoresObj: { scores } }) => {
           <h1>Μόρια Στελεχών</h1>
         </div>
         <hr />
-        <h3 className="mt-3"></h3>
-        <div className="card">
-          <div className="card-header">
-            <h3>Αναλυτικός πίνακας στελεχών που μοριοδοτούνται: </h3>
-          </div>
-          <div className="card-body">
-            <table className="table table-responsive-sm table-striped">
-              <thead>
-                <tr>
-                  <th>
-                    Α<span>/</span>A
-                  </th>
-                  <th>Βαθμός</th>
-                  <th>Όνομα</th>
-                  <th>Έπώνυμο</th>
-                  <th>Μονάδα</th>
-                  <th>Οικογ. Κατάσταση</th>
-                  <th>Παιδιά</th>
-                  {/*<th>Μόρια Βαθμόυ</th>*/}
-                  {/*<th>Μόρια Συζύγου</th>*/}
-                  {/*<th>*/}
-                  {/*  1<sup>ου</sup> Παδιού*/}
-                  {/*</th>*/}
-                  {/*<th>*/}
-                  {/*  3<sup>ου</sup> 4<sup>ου</sup> Παδιού Και Άνω*/}
-                  {/*</th>*/}
-                  <th>Μέλος Οικογ. Με Ειδικές Ανάγκες</th>
-                  {/*<th>Μήνες Αναμονής Χ1</th>*/}
-                  {/*<th>Μήνες Αναμονής Χ2</th>*/}
-                  <th>Σύνολο Θετικών Μορίων</th>
-                  {/*<th>Μήνες Προηγ. Στέγασης Χ2</th>*/}
-                  {/*<th>Μήνες Προηγ. Στέγασης Χ3</th>*/}
-                  {/*<th>Χρ. Υπηρετ. Στο Εξωτ.</th>*/}
-                  {/*<th>Ετήσιο Εισόδημα</th>*/}
-                  <th>Σύνολο Αρνητικών Μορίων</th>
-                  <th>Γενικό Σύνολο Μορίων</th>
-                  <th>Επιθυμία Στέγασης</th>
-                </tr>
-              </thead>
-              <tbody>
-                {scores.map((score, index) => (
-                  <tr key={uuidv4()}>
-                    <td key={uuidv4()}>{index + 1}</td>
-                    <td
-                      key={uuidv4()}
-                    >{`${score.personInArmy.rank} ${score.personInArmy.specialty}`}</td>
-                    <td key={uuidv4()}>{score.personInArmy.firstname}</td>
-                    <td key={uuidv4()}>{score.personInArmy.surname}</td>
-                    <td key={uuidv4()}>
-                      {score.personInArmy.unit
-                        ? score.personInArmy.unit
-                        : "Δεν Έχει Οριστεί"}
-                    </td>
-                    <td key={uuidv4()}>
-                      {score.isMarried ? "Έγγαμος" : "Άγαμος"}
-                    </td>
-                    <td key={uuidv4()}>{score.hasNumOfChildren}</td>
-                    <td key={uuidv4()}>
-                      {score.hasRelativeWithDisability ? "Ναί" : "Όχι"}
-                    </td>
-                    <td key={uuidv4()}>0</td>
-                    <td key={uuidv4()}>0</td>
-                    <td key={uuidv4()}>{score.score}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-header">
+                <h3>Αναλυτικός πίνακας στελεχών που μοριοδοτούνται: </h3>
+              </div>
+              <div className="card-body table-responsive">
+                <table className="table table-striped table-hover">
+                  <thead>
+                    <tr>
+                      <th>
+                        Α<span>/</span>A
+                      </th>
+                      <th>Βαθμός</th>
+                      <th className="hidden-md">Όνομα</th>
+                      <th>Έπώνυμο</th>
+                      <th>Μονάδα</th>
+                      <th>Οικογ. Κατάσταση</th>
+                      <th>Παιδιά</th>
+                      {/*<th>Μόρια Βαθμόυ</th>*/}
+                      {/*<th>Μόρια Συζύγου</th>*/}
+                      {/*<th>*/}
+                      {/*  1<sup>ου</sup> Παδιού*/}
+                      {/*</th>*/}
+                      {/*<th>*/}
+                      {/*  3<sup>ου</sup> 4<sup>ου</sup> Παδιού Και Άνω*/}
+                      {/*</th>*/}
+                      <th>Μέλος Οικογ. Με Ειδικές Ανάγκες</th>
+                      {/*<th>Μήνες Αναμονής Χ1</th>*/}
+                      {/*<th>Μήνες Αναμονής Χ2</th>*/}
+                      <th>Σύνολο Θετικών Μορίων</th>
+                      {/*<th>Μήνες Προηγ. Στέγασης Χ2</th>*/}
+                      {/*<th>Μήνες Προηγ. Στέγασης Χ3</th>*/}
+                      {/*<th>Χρ. Υπηρετ. Στο Εξωτ.</th>*/}
+                      {/*<th>Ετήσιο Εισόδημα</th>*/}
+                      <th>Σύνολο Αρνητικών Μορίων</th>
+                      <th>Γενικό Σύνολο Μορίων</th>
+                      <th>Επιθυμία Στέγασης</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {scores.map((score, index) => (
+                      <tr key={uuidv4()}>
+                        <td key={uuidv4()}>{index + 1}</td>
+                        <td
+                          key={uuidv4()}
+                        >{`${score.personInArmy.rank} ${score.personInArmy.specialty}`}</td>
+                        <td className="hidden-md" key={uuidv4()}>
+                          {score.personInArmy.firstname}
+                        </td>
+                        <td key={uuidv4()}>{score.personInArmy.surname}</td>
+                        <td key={uuidv4()}>
+                          {score.personInArmy.unit
+                            ? score.personInArmy.unit
+                            : "Δεν Έχει Οριστεί"}
+                        </td>
+                        <td key={uuidv4()}>
+                          {score.isMarried ? "Έγγαμος" : "Άγαμος"}
+                        </td>
+                        <td key={uuidv4()}>{score.hasNumOfChildren}</td>
+                        <td key={uuidv4()}>
+                          {score.hasRelativeWithDisability ? "Ναί" : "Όχι"}
+                        </td>
+                        <td key={uuidv4()}>0</td>
+                        <td key={uuidv4()}>0</td>
+                        <td key={uuidv4()}>{score.score}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </div>
